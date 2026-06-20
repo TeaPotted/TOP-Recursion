1c5b9c8fc64ad4f79d38905b756d99c3bf8c9f6b
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-06-20 21:23:02 +0800

    create tests for fibsRec() function

--- Fibonacci/fibonacci.test.js
+++ Fibonacci/fibonacci.test.js
@@ -1,4 +1,4 @@
-import {fibs} from "./fibonacci.js"
+import {fibs, fibsRec} from "./fibonacci.js"
 
 test("fibs(8) returns [0, 1, 1, 2, 3, 5, 8, 13]", () => {
   expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
@@ -6,4 +6,16 @@ test("fibs(8) returns [0, 1, 1, 2, 3, 5, 8, 13]", () => {
 
 test("fibs(1) returns [1]", () => {
   expect(fibs(1)).toEqual([0])
+})
+
+test("fibsRec(8) returns [0, 1, 1, 2, 3, 5, 8, 13]", () => {
+  expect(fibsRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+})
+
+test("fibsRec(1) returns [1]", () => {
+  expect(fibsRec(1)).toEqual([0])
+})
+
+test("fibsRec(0) return [0]", () => {
+  expect(fibsRec(0)).toEqual([0])
 })
